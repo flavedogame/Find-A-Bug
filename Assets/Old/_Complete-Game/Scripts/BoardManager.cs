a0e52f2a696c72e88986167f9b2dae2ca3e87b08
shshwdr <cs.vivian04@gmail.com>
2018-09-21 00:22:14 -0700

    add UI for item state

--- Assets/Old/_Complete-Game/Scripts/BoardManager.cs
+++ Assets/Old/_Complete-Game/Scripts/BoardManager.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿ using UnityEngine;
 using System;
 using System.Collections.Generic; 		//Allows us to use Lists.
 using Random = UnityEngine.Random; 		//Tells Random to use the Unity Engine random number generator.
