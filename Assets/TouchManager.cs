1dc7a899518df943ea4e8b320ded71ebbb3b5c3e
shshwdr <cs.vivian04@gmail.com>
2018-09-22 23:45:56 -0700

    find out when hit on player

---
+++ Assets/TouchManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (GameLogicManager.Instance.isPaused)
+        //{
+        //    return;
+        //}
+        //#if !UNITY_EDITOR
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase != TouchPhase.Began)
+            {
+                return;
+            }
+            //Your raycast handling
+            Vector3 mousePosition = touch.position;
+            //CSUtil.LOG("screen touched " + mousePosition);
+            mousePosition = new Vector3(mousePosition.x, mousePosition.y, 10);
+            RaycastHit2D[] vHits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(mousePosition), Vector2.zero);
+            foreach (RaycastHit2D vhit in vHits)
+            {
+                if (vhit.transform.tag == "BugableObject")
+                {
+                    Debug.Log("tapped buagable object");
+                }
+            }
+        }
+        //#endif
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Your raycast handling
+            Vector3 mousePosition = Input.mousePosition;
+            //CSUtil.LOG("mouse click " + mousePosition);
+            mousePosition = new Vector3(mousePosition.x, mousePosition.y, 10);
+            RaycastHit2D[] vHits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(mousePosition), Vector2.zero);
+            foreach (RaycastHit2D vhit in vHits)
+            {
+                if (vhit.transform.tag == "BugableObject" && vhit.collider.isTrigger)
+                {
+                    //Debug.Log("tapped buagable object");
+                }
+            }
+        }
+    }
+}
