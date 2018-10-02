19e6fa2bb515cc33415886b682d984bf5d961981
shshwdr <cs.vivian04@gmail.com>
2018-10-02 00:06:47 -0700

    UI for developer dialog

---
+++ Assets/DeveloperDialogViewController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DeveloperDialogViewController : MonoBehaviour
+{
+
+    public TextMeshProUGUI dialogText;
+    public Image dialogIcon;
+    // Start is called before the first frame update
+
+    public void Init()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
