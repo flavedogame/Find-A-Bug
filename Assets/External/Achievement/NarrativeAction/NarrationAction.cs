9b3aaf1a6cdcbc51c13f51bd50b8cb6a347e304a
shshwdr <cs.vivian04@gmail.com>
2018-10-02 01:28:07 -0700

    add csv for narration

---
+++ Assets/External/Achievement/NarrativeAction/NarrationAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarrationAction : NarrativeAction
+{
+        public NarrationAction(NarrativeInfo info) : base(info)
+        {
+
+        }
+
+        protected override void P_Enable()
+        {
+            Debug.Log("enable tutorial action with info: " + narrativeInfo);
+        }
+    }
