a8b6b37bc818bfa394f2ab72a2515bedad5e84f0
shshwdr <cs.vivian04@gmail.com>
2018-10-09 02:31:18 -0700

    support cursor change when hover on different things

--- Assets/GameViewController.cs
+++ Assets/GameViewController.cs
@@ -10,7 +10,6 @@ public class GameViewController : MonoBehaviour {
     public Button pauseButton;
     public Button foundBugButton;
     public TextMeshProUGUI foundBugButtonText;
-    public Texture2D cursorTexture;
 
     // Use this for initialization
     void Start () {
@@ -40,12 +39,11 @@ public class GameViewController : MonoBehaviour {
         if (GameModeManager.Instance.isInFindBugMode)
         {
             foundBugButtonText.text = "Back To Play";
-            Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+            
         }
         else
         {
             foundBugButtonText.text = "FOUND IT!";
-            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
         }
     }
 	
--- Assets/TouchManager.cs
+++ Assets/TouchManager.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TouchManager : MonoBehaviour {
+public class TouchManager : MonoBehaviour
+{
 
+    public Texture2D cursorTexture;
+    public Texture2D cursorTextureHit;
+
+    enum CursorStateEnum { playMode, findBugNormal, findBugHit };
+    CursorStateEnum cursorState;
 
     // Update is called once per frame
     void Update()
@@ -13,55 +19,89 @@ public class TouchManager : MonoBehaviour {
         //{
         //    return;
         //}
-        //#if !UNITY_EDITOR
-        if (EventSystem.current.IsPointerOverGameObject())
-        {
-            return;
-        }
-        if (Input.touchCount == 1)
-        {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase != TouchPhase.Began)
-            {
-                return;
-            }
-            //Your raycast handling
-            Vector3 mousePosition = touch.position;
-            //CSUtil.LOG("screen touched " + mousePosition);
-            mousePosition = new Vector3(mousePosition.x, mousePosition.y, 10);
-            RaycastHit2D[] vHits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(mousePosition), Vector2.zero);
-            bool getHit = false;
-            foreach (RaycastHit2D vhit in vHits)
-            {
-                if (vhit.transform.tag == "BugableObject")
-                {
-                    BugableObject script = vhit.transform.gameObject.GetComponent<BugableObject>();
-                    script.DidTap();
-                    getHit = true;
-                }
-            }
-            if(getHit == false)
-            {
-                //should not do this here, but we should show different cursor when it is on something that is a bug object to when it is not
-            }
-        }
+        //#if !UNITY_EDITORs
+        //if (Input.touchCount == 1)
+        //{
+        //    Touch touch = Input.GetTouch(0);
+        //    if (touch.phase != TouchPhase.Began)
+        //    {
+        //        return;
+        //    }
+        //    //Your raycast handling
+        //    Vector3 mousePosition = touch.position;
+        //    //CSUtil.LOG("screen touched " + mousePosition);
+        //    mousePosition = new Vector3(mousePosition.x, mousePosition.y, 10);
+        //    RaycastHit2D[] vHits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(mousePosition), Vector2.zero);
+        //    bool getHit = false;
+        //    foreach (RaycastHit2D vhit in vHits)
+        //    {
+        //        if (vhit.transform.tag == "BugableObject")
+        //        {
+        //            BugableObject script = vhit.transform.gameObject.GetComponent<BugableObject>();
+        //            script.DidTap();
+        //            getHit = true;
+        //        }
+        //    }
+        //    if(getHit == false)
+        //    {
+        //        //should not do this here, but we should show different cursor when it is on something that is a bug object to when it is not
+        //    }
+        //}
         //#endif
 
-        if (Input.GetMouseButtonDown(0))
+
+        // Your raycast handling
+        Vector3 mousePosition = Input.mousePosition;
+        //CSUtil.LOG("mouse click " + mousePosition);
+        mousePosition = new Vector3(mousePosition.x, mousePosition.y, 10);
+        RaycastHit2D[] vHits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(mousePosition), Vector2.zero);
+        bool hasHitted = false;
+        foreach (RaycastHit2D vhit in vHits)
         {
-            // Your raycast handling
-            Vector3 mousePosition = Input.mousePosition;
-            //CSUtil.LOG("mouse click " + mousePosition);
-            mousePosition = new Vector3(mousePosition.x, mousePosition.y, 10);
-            RaycastHit2D[] vHits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(mousePosition), Vector2.zero);
-            foreach (RaycastHit2D vhit in vHits)
+            if (vhit.transform.tag == "BugableObject" && vhit.collider.isTrigger)
             {
-                if (vhit.transform.tag == "BugableObject" && vhit.collider.isTrigger)
+
+                if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
                 {
                     BugableObject script = vhit.transform.gameObject.GetComponent<BugableObject>();
                     script.DidTap();
                 }
+                //improve this later, don't call manager in update
+                if (GameModeManager.Instance.isInFindBugMode)
+                {
+                    ChangeCursorState(CursorStateEnum.findBugHit);
+                    hasHitted = true;
+                }
             }
+        }
+        if (!GameModeManager.Instance.isInFindBugMode)
+        {
+            ChangeCursorState(CursorStateEnum.playMode);
+        }
+        else if (!hasHitted)
+        {
+            ChangeCursorState(CursorStateEnum.findBugNormal);
+        }
+
+    }
+
+    void ChangeCursorState(CursorStateEnum newState)
+    {
+        if (cursorState != newState)
+        {
+            switch (newState)
+            {
+                case CursorStateEnum.findBugNormal:
+                    Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+                    break;
+                case CursorStateEnum.findBugHit:
+                    Cursor.SetCursor(cursorTextureHit, Vector2.zero, CursorMode.Auto);
+                    break;
+                case CursorStateEnum.playMode:
+                    Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+                    break;
+            }
+            cursorState = newState;
         }
     }
 }
