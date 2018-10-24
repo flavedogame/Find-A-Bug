fe687dcaf8de5b5ee54f7203fd84fe718e562ea9
shshwdr <cs.vivian04@gmail.com>
2018-10-24 03:31:47 -0700

    add hp bar

---
+++ Assets/GameHealthObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHealthObject : MonoBehaviour
+{
+    public int maxHP = 2;
+    int currentHP;
+
+
+
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/GameMonster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMonster : GameHealthObject
+{
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
